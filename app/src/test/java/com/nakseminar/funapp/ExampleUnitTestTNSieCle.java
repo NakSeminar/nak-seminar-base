e189a551fdb58a92b89678ad17c06a063f738997
NakTNSieCle <naktn.siecle@gmx.de>
2017-01-10 10:39:43 +0000

    Commit 1 to 3

--- app/src/test/java/com/nakseminar/funapp/ExampleUnitTestTNSieCle.java
+++ app/src/test/java/com/nakseminar/funapp/ExampleUnitTestTNSieCle.java
@@ -7,11 +7,12 @@ import static org.junit.Assert.*;
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
+ *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
+public class ExampleUnitTestTNSieCle {
     @Test
     public void addition_isCorrect() throws Exception {
-        assertEquals(12, 6 + 6);
+        assertEquals(13, 6 + 7);
     }
 }
