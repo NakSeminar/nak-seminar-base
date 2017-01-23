e08098caff5cb9451bc5b182b588d16c2539ff00
NakTNSieCle <naktn.siecle@gmx.de>
2017-01-23 21:13:59 +0100

    try ssh

--- gradlew.bat
+++ gradlew.bat
@@ -3,7 +3,7 @@
 @rem
 @rem  Gradle startup script for Windows
 @rem
-@rem test23 #########################################################################
+@rem #########################################################################
 
 @rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
