c45467b8d05e1f2226d120edae4544ed674e9689
Hans Rißer <hans.r.1999@gmail.com>
2019-05-18 14:36:57 +0200

    Added a funny new Main Class

---
+++ app/src/test/java/com/nakseminar/funapp/FunnyMain.java
@@ -0,0 +1,16 @@
+
+/**
+ * @author Hans R
+ * */
+public class FunnyMain {
+
+	static {
+		System.out.println("I don't even need my main");
+	}
+
+
+	public static void main(String[] args) {
+		// emptyOD
+	}
+
+}
