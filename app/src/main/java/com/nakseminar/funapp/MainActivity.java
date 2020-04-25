f3a367eda3c00fb3c5f0d0144c2152c3b2f975bf
Tristan <jan_tristan.heisecke@nordakademie.de>
2020-04-25 16:43:58 +0200

    :)

--- app/src/main/java/com/nakseminar/funapp/MainActivity.java
+++ app/src/main/java/com/nakseminar/funapp/MainActivity.java
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-		//hi from the seminar
+		//hi from the seminar :)
         setContentView(R.layout.activity_main);
     }
 }
